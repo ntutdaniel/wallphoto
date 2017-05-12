788731e6a6dd485835d8dd7d588e1bb3ecd026c7
tangdaniel <s102590041@gmail.com>
2017-05-12 17:26:37 +0800

    login view

---
+++ photoWall/View/LogInViewController.h
@@ -0,0 +1,13 @@
+//
+//  LogInViewController.h
+//  photoWall
+//
+//  Created by tangdaniel on 12/05/2017.
+//  Copyright © 2017 edu.self. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface LogInViewController : UIViewController
+
+@end
